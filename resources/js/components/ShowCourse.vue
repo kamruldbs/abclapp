96c0db1447e8a2b6e30eed7e945022934cc49fc4
Kamrul DBSL <mkamrulislam.dbs@gmail.com>
2021-09-07 19:32:38 +0600

    environment setup

---
+++ resources/js/components/ShowCourse.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello From Component</h1>
+</template>
+
+<script>
+
+    export default {
+        props:{
+        },
+        data:()=>({
+
+        }),
+
+        created(){
+
+        },
+        methods:{
+
+        },
+    }
+</script>
---
+++ resources/views/course/showCourse.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.theme')
+
+@section('content')
+<div id="app">
+    <show-course></show-course>
+        {{-- <h1>Hello From Component</h1> --}}
+        {{-- @yield('content') --}}
+        {{-- <example-component></example-component> --}}
+</div>
+
+@endsection
