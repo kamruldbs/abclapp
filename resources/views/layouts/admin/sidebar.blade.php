f433d3cc3d796f6d74c7a2a6cb21fabfd4d5963f
Kamrul DBSL <mkamrulislam.dbs@gmail.com>
2021-09-08 14:59:40 +0600

    Logout added

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -100,9 +100,29 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./customers" class="nav-link">
+                <a href="{{route('courseEntry')}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Customers Entry</p>
+                  <p>Course Entry</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Profile
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>{{ __('Logout') }}</p>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                  </form>
                 </a>
               </li>
             </ul>
